a02153477b32377723d45e66b779971b3339a18a
Andy Blyth <andy@technicaldogsbody.com>
2016-01-19 09:52:12 +0000

    This is how very rough untested code, this is how is shouldn't be done!

---
+++ DDD.Solid.DI.Example/DDD.Solid.DI.Example.One/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Solid.DI.Example.One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                double? number = null;
+                double temp;
+                while (number == null)
+                {
+                    Console.WriteLine("Please enter a number:");
+
+                    if (double.TryParse(Console.ReadLine(), out temp))
+                    {
+                        number = temp;
+                    }
+
+                }
+
+                var response = DecimalNumberWords(number.Value);
+
+                Console.WriteLine("String Value: " + response);
+
+            }
+
+
+        }
+
+        private static string DecimalNumberWords(double n)
+        {
+            string words = "";
+            double intPart;
+            double decPart = 0;
+            if (n == 0)
+                return "zero";
+            try
+            {
+                string[] splitter = n.ToString().Split('.');
+                intPart = double.Parse(splitter[0]);
+                decPart = double.Parse(splitter[1]);
+            }
+            catch
+            {
+                intPart = n;
+            }
+
+            words = WholeNumberWords(intPart);
+
+            if (decPart > 0)
+            {
+                if (words != "")
+                    words += " and ";
+                int counter = decPart.ToString().Length;
+                switch (counter)
+                {
+                    case 1: words += WholeNumberWords(decPart) + " tenths"; break;
+                    case 2: words += WholeNumberWords(decPart) + " hundredths"; break;
+                    case 3: words += WholeNumberWords(decPart) + " thousandths"; break;
+                    case 4: words += WholeNumberWords(decPart) + " ten-thousandths"; break;
+                    case 5: words += WholeNumberWords(decPart) + " hundred-thousandths"; break;
+                    case 6: words += WholeNumberWords(decPart) + " millionths"; break;
+                    case 7: words += WholeNumberWords(decPart) + " ten-millionths"; break;
+                }
+            }
+            return words;
+        }
+
+        private static string WholeNumberWords(double n) //converts double to words
+        {
+            string[] numbersArr = new string[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+            string[] tensArr = new string[] { "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty" };
+            string[] suffixesArr = new string[] { "thousand", "million", "billion", "trillion", "quadrillion", "quintillion", "sextillion", "septillion", "octillion", "nonillion", "decillion", "undecillion", "duodecillion", "tredecillion", "Quattuordecillion", "Quindecillion", "Sexdecillion", "Septdecillion", "Octodecillion", "Novemdecillion", "Vigintillion" };
+            string words = "";
+
+            bool tens = false;
+
+            if (n < 0)
+            {
+                words += "negative ";
+                n *= -1;
+            }
+
+            int power = (suffixesArr.Length + 1) * 3;
+
+            while (power > 3)
+            {
+                double pow = Math.Pow(10, power);
+                if (n >= pow)
+                {
+                    if (n % pow > 0)
+                    {
+                        words += WholeNumberWords(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1] + ", ";
+                    }
+                    else if (n % pow == 0)
+                    {
+                        words += WholeNumberWords(Math.Floor(n / pow)) + " " + suffixesArr[(power / 3) - 1];
+                    }
+                    n %= pow;
+                }
+                power -= 3;
+            }
+            if (n >= 1000)
+            {
+                if (n % 1000 > 0) words += WholeNumberWords(Math.Floor(n / 1000)) + " thousand, ";
+                else words += WholeNumberWords(Math.Floor(n / 1000)) + " thousand";
+                n %= 1000;
+            }
+            if (0 <= n && n <= 999)
+            {
+                if ((int)n / 100 > 0)
+                {
+                    words += WholeNumberWords(Math.Floor(n / 100)) + " hundred";
+                    n %= 100;
+                }
+                if ((int)n / 10 > 1)
+                {
+                    if (words != "")
+                        words += " ";
+                    words += tensArr[(int)n / 10 - 2];
+                    tens = true;
+                    n %= 10;
+                }
+
+                if (n < 20 && n > 0)
+                {
+                    if (words != "" && tens == false)
+                        words += " ";
+                    words += (tens ? "-" + numbersArr[(int)n - 1] : numbersArr[(int)n - 1]);
+                    n -= Math.Floor(n);
+                }
+            }
+
+            return words;
+
+        }
+    }
+}
